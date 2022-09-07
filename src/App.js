0e1c29f7658d069853fe5f74ffeabfd65e03fbba
Carla Vilches <carluvilches145@gmail.com>
2022-09-07 20:59:21 -0300

    Menu e-commerce

--- src/App.js
+++ src/App.js
@@ -3,22 +3,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav class="navbar navbar-expand-lg bg-ligth shadow">
+    <div class="container">
+      <a class="navbar-brand">JoyShop</a>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <div class="navbar-nav">
+          <a class="nav-link">Anillos</a>
+          <a class="nav-link" >Aros</a>
+          <a class="nav-link" >Pulseras</a>
+          <a class="nav-link" >Collares</a>
+        </div>
+      </div>
     </div>
+  </nav>
   );
 }
 
